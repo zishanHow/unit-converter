985aceaface5ad3ea0baaa9a37d2fafce35c971c
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-09-21 23:30:47 +0600

    add Celsius and Fahrenheit.

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const convertBtn = document.getElementById("convert-btn")
 const lengthPara = document.getElementById("length-para")
 const volumePara = document.getElementById("volume-para")
 const massPera = document.getElementById("mass-para")
+const temperaturePera = document.getElementById("temperature-para")
 
 
 convertBtn.addEventListener("click", function () {
     length()
     volume()
     mass()
+    temperature()
 })
 
 function length() {
@@ -35,4 +37,12 @@ function mass() {
     const poundToKilos = inputValue / 2.205
 
     massPera.textContent = `${inputValue} kilos = ${kilosToPound.toFixed(3)} pounds | ${inputValue} pounds = ${poundToKilos.toFixed(3)} kilos`
+}
+
+function temperature() {
+    let inputValue = inputNum.value
+    const celsiusToFahrenheit = (inputValue * 9/5) + 32
+    const fahrenheitToCelsius = (inputValue - 32) * 5/9
+
+    temperaturePera.textContent = `${inputValue} Celsius = ${celsiusToFahrenheit.toFixed(3)} Fahrenheit | ${inputValue} Fahrenheit = ${fahrenheitToCelsius.toFixed(3)} Celsius`
 }
