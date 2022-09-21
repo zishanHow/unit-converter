bd290dc67c51ca34a049940fdd8a8367c0699526
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-09-22 01:43:00 +0600

    update style
    update everything make it more stylish.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
 let inputNum = document.getElementById("input-num")
 const convertBtn = document.getElementById("convert-btn")
 const lengthPara = document.getElementById("length-para")
 const volumePara = document.getElementById("volume-para")
 const massPera = document.getElementById("mass-para")
 const temperaturePera = document.getElementById("temperature-para")
-
 
 convertBtn.addEventListener("click", function () {
     length()
@@ -19,7 +17,7 @@ function length() {
     const meterToFeet = inputValue * 3.281
     const feetToMeter = inputValue / 3.281
 
-    lengthPara.textContent = `${inputValue} meters = ${meterToFeet.toFixed(3)} feet | ${inputValue} feet = ${feetToMeter.toFixed(3)} meters`
+    lengthPara.textContent = `${inputValue} Meters = ${meterToFeet.toFixed(3)} Feet | ${inputValue} Feet = ${feetToMeter.toFixed(3)} Meters`
 }
 
 function volume() {
@@ -27,7 +25,7 @@ function volume() {
     const literToGallon = inputValue / 3.785
     const gallonToLiter = inputValue * 3.785
 
-    volumePara.textContent = `${inputValue} liters = ${literToGallon.toFixed(3)} gallons | ${inputValue} gallons = ${gallonToLiter.toFixed(3)} liters`
+    volumePara.textContent = `${inputValue} Liters = ${literToGallon.toFixed(3)} Gallons | ${inputValue} Gallons = ${gallonToLiter.toFixed(3)} Liters`
 }
 
 
@@ -36,13 +34,13 @@ function mass() {
     const kilosToPound = inputValue * 2.205
     const poundToKilos = inputValue / 2.205
 
-    massPera.textContent = `${inputValue} kilos = ${kilosToPound.toFixed(3)} pounds | ${inputValue} pounds = ${poundToKilos.toFixed(3)} kilos`
+    massPera.textContent = `${inputValue} Kilos = ${kilosToPound.toFixed(3)} Pounds | ${inputValue} Pounds = ${poundToKilos.toFixed(3)} Kilos`
 }
 
 function temperature() {
     let inputValue = inputNum.value
-    const celsiusToFahrenheit = (inputValue * 9/5) + 32
-    const fahrenheitToCelsius = (inputValue - 32) * 5/9
+    const celsiusToFahrenheit = (inputValue * 9 / 5) + 32
+    const fahrenheitToCelsius = (inputValue - 32) * 5 / 9
 
-    temperaturePera.textContent = `${inputValue} Celsius = ${celsiusToFahrenheit.toFixed(3)} Fahrenheit | ${inputValue} Fahrenheit = ${fahrenheitToCelsius.toFixed(3)} Celsius`
+    temperaturePera.textContent = `${inputValue}°C = ${celsiusToFahrenheit.toFixed(3)} Fahrenheit | ${inputValue}°F = ${fahrenheitToCelsius.toFixed(3)} Celsius`
 }
